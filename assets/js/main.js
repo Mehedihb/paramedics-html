e1f827c526fa2427015ae2b3eb59116f62ff483a
Md Mehedi Hassan <mehedi.all.info@gmail.com>
2024-01-19 23:47:37 +0600

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,24 +84,7 @@
 		$(this).css("background-color", $(this).attr("data-bg-color"));
 	});
 	
-	/*======================================
-	 Cart Quantity Js
-	========================================*/
-	$(".cart-minus").click(function () {
-		var $input = $(this).parent().find("input");
-		var count = parseInt($input.val()) - 1;
-		count = count < 1 ? 1 : count;
-		$input.val(count);
-		$input.change();
-		return false;
-	});
 
-	$(".cart-plus").click(function () {
-		var $input = $(this).parent().find("input");
-		$input.val(parseInt($input.val()) + 1);
-		$input.change();
-		return false;
-	});
 
 	/*======================================
 	MagnificPopup image view
@@ -166,20 +149,6 @@
 		return false;
 	})
 
-	/*======================================
-	For language Js
-	========================================*/
-	if ($("#header-lang-toggle").length > 0) {
-		window.addEventListener('click', function(e){
-	
-			if (document.getElementById('header-lang-toggle').contains(e.target)){
-				$(".header-lang ul").toggleClass("lang-list-open");
-			}
-			else{
-				$(".header-lang ul").removeClass("lang-list-open");
-			}
-		});
-	}
 
 	/*======================================
 	For header header Js
