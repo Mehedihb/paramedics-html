e379d405ba659035b778cd37e7020266b4c10073
Md Mehedi Hassan <mehedi.all.info@gmail.com>
2024-04-22 00:28:08 +0600

    Team Swiper Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,40 @@
-
 // Sticky Header - Start
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   var scrollTop = window.scrollY || document.documentElement.scrollTop;
 
   if (scrollTop > 0) {
-    document.querySelector('.site_header').classList.add("sticky");
+    document.querySelector(".site_header").classList.add("sticky");
   } else {
-    document.querySelector('.site_header').classList.remove("sticky");
+    document.querySelector(".site_header").classList.remove("sticky");
   }
 });
 // Sticky Header - End
 
 // Swiper Slider - Start
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
+
+var swiper = new Swiper(".memberSwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
   pagination: {
